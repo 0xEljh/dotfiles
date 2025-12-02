beab86672c4f7a66e03f2d91854f80f7ea339eaf
elijah <elijahng96@gmail.com>
2025-12-02 18:21:26 -0500

    update ff keymapping

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Swap keymaps for telescope's find files (cwd vs root dir)
+vim.keymap.del("n", "<leader>ff")
+vim.keymap.del("n", "<leader>fF")
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files cwd=.<cr>", { desc = "Find Files (cwd)" })
+vim.keymap.set("n", "<leader>fF", function()
+  require("lazyvim.util").telescope("files", { cwd = false })()
+end, { desc = "Find Files (Root Dir)" })
