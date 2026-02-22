f76682ce5cbf801ad11d157ce6d1087e9b786089
0xEljh <elijah@0xeljh.com>
2026-02-22 19:23:00 +0000

    update neovim keymap: swap j/k

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Swap j/k for up/down
+vim.keymap.set({ "n", "v", "o" }, "j", "k", { desc = "Up" })
+vim.keymap.set({ "n", "v", "o" }, "k", "j", { desc = "Down" })
+vim.keymap.set({ "n", "v", "o" }, "gj", "gk", { desc = "Up (wrapped line)" })
+vim.keymap.set({ "n", "v", "o" }, "gk", "gj", { desc = "Down (wrapped line)" })
+
 -- Swap keymaps for telescope's find files (cwd vs root dir)
 vim.keymap.del("n", "<leader>ff")
 vim.keymap.del("n", "<leader>fF")
